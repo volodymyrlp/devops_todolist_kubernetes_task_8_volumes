5d670c56c99e7eb6ebe9b4dfc35e130109a6c94d
Volodymyr Lysenko <mr.volodumyr@gmail.com>
2026-06-14 03:28:14 +0000

    Solution

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 mkdir -p /tmp/todoapp-data
 
 kubectl apply -f .infrastructure/namespace.yml
-kubectl apply -f .infrastructure/confgiMap.yml
+kubectl apply -f .infrastructure/configMap.yml
 kubectl apply -f .infrastructure/secret.yml
 kubectl apply -f pv.yml
 kubectl apply -f pvc.yml
